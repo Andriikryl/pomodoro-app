0b5152e9b78e71378543960ac0646fada74bd6b0
Amdrii <andreyk557@gmail.com>
2023-03-19 15:21:34 +0200

    feat[play-btn]

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,25 @@
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import PlayButton from "./PlayButton";
+
+const red = "#e63946";
+const green = "#3a5a40";
 
 function Timer() {
   return (
     <div>
-      <CircularProgressbar value={60} text={`60%`} />;
+      <CircularProgressbar
+        value={60}
+        text={`60%`}
+        styles={buildStyles({
+          textColor: "#fff",
+          pathColor: red,
+          trailColor: "#f1faee",
+        })}
+      />
+      <div>
+        <PlayButton />
+      </div>
     </div>
   );
 }
